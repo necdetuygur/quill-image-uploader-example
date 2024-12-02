9554a1c0c3d48bba14f947c44acedc53c3454bb3
Necdet UYGUR <necdet.uygur@gmail.com>
2024-12-02 14:04:29 +0300

    Random filename generated.

--- server.js
+++ server.js
@@ -1,34 +1,38 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 // Multer konfigürasyonu
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); // Dosya adı olarak timestamp kullanıyoruz
-  }
+    const filename = `${Date.now()}${~~(
+      Math.random() * (9999 - 1000) +
+      1000
+    )}${path.extname(file.originalname)}`;
+    cb(null, filename);
+  },
 });
 
 const upload = multer({ storage: storage });
 
 // Statik dosyalar için middleware
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // GET isteği geldiğinde index.html dosyasını döndür
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // POST isteği ile resim yükleme
-app.post('/upload', upload.single('image'), (req, res) => {
+app.post("/upload", upload.single("image"), (req, res) => {
   if (!req.file) {
-    return res.status(400).send('No file uploaded.');
+    return res.status(400).send("No file uploaded.");
   }
   res.json({ url: `/uploads/${req.file.filename}` });
 });
